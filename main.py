70f39f6664f0dd28d862046914664a9a93bc2591
igor <isusmelj@gmail.com>
2017-07-05 14:35:57 +0200

    initial commit of cleaned up repository

---
+++ main.py
@@ -0,0 +1,123 @@
+import os
+import scipy.misc
+import numpy as np
+
+from model import DCGAN
+from utils import pp, visualize, to_json, show_all_variables, generate_image_from_z
+
+import tensorflow as tf
+
+flags = tf.app.flags
+flags.DEFINE_integer("epoch", 25, "Epoch to train [25]")
+flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
+flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
+flags.DEFINE_integer("train_size", np.inf, "The size of train images [np.inf]")
+flags.DEFINE_integer("batch_size", 64, "The size of batch images [64]")
+flags.DEFINE_integer("input_height", 108, "The size of image to use (will be center cropped). [108]")
+flags.DEFINE_integer("input_width", None, "The size of image to use (will be center cropped). If None, same value as input_height [None]")
+flags.DEFINE_integer("output_height", 64, "The size of the output images to produce [64]")
+flags.DEFINE_integer("output_width", None, "The size of the output images to produce. If None, same value as output_height [None]")
+flags.DEFINE_string("dataset", "celebA", "The name of dataset [celebA, mnist, lsun]")
+flags.DEFINE_string("input_fname_pattern", "*.jpg", "Glob pattern of filename of input images [*]")
+flags.DEFINE_string("checkpoint_dir", "checkpoint", "Directory name to save the checkpoints [checkpoint]")
+flags.DEFINE_boolean("train", False, "True for training, False for testing [False]")
+flags.DEFINE_boolean("crop", False, "True for training, False for testing [False]")
+flags.DEFINE_boolean("visualize", False, "True for visualizing, False for nothing [False]")
+
+# Custom flags
+flags.DEFINE_string("folder_suffix", "", "Suffix of folder names (logs, samples, checkpoints), []")
+flags.DEFINE_string("inference_z_src", "_", "Name of checkpoint file suffix to generate image from []")
+flags.DEFINE_string("sample_dir", "samples", "Directory name to save the image samples [samples]")
+flags.DEFINE_integer("GpD_ratio", -1, "Generator per discriminator training iteration steps. -1 uses the controller [-1]")
+flags.DEFINE_string("blur_strategy", "reg_hyp", "Blur strategy to use for training. [None, 3x3, reg_lin, reg_hyp]")
+flags.DEFINE_boolean("with_overlay", False, "If True, an overlay indicating the realism from the discriminators point of view will be added to all images in a batch. Additionally each batch will be sorted using this realism. Most realistic image on bottom right. [False]")
+flags.DEFINE_integer("sample_every", 1000, "Sample a new image all x iterations during training. [1000]")
+
+# For the Controller
+flags.DEFINE_float("target_starting_G_quality", 0.25, "Target quality of generator in the beginning of the training for the controller [0.25]")
+flags.DEFINE_float("target_ending_G_quality", 0.25, "Target quality of generator in the end of the training for the controller [0.25]")
+flags.DEFINE_float("control_gain", 0.001, "Gain of the controller [0.001]")
+
+FLAGS = flags.FLAGS
+
+def main(_):
+  pp.pprint(flags.FLAGS.__flags)
+
+  if FLAGS.input_width is None:
+    FLAGS.input_width = FLAGS.input_height
+  if FLAGS.output_width is None:
+    FLAGS.output_width = FLAGS.output_height
+  
+  folder_suffix = FLAGS.folder_suffix
+  checkpoint_dir = FLAGS.checkpoint_dir + folder_suffix
+  sample_dir = FLAGS.sample_dir + folder_suffix
+  log_dir = './logs' + folder_suffix
+
+  if not os.path.exists(checkpoint_dir):
+    os.makedirs(checkpoint_dir)
+  if not os.path.exists(sample_dir):
+    os.makedirs(sample_dir)
+
+  #gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+  run_config = tf.ConfigProto()
+  run_config.gpu_options.allow_growth=True
+  run_config.allow_soft_placement=True
+
+  with tf.Session(config=run_config) as sess:
+    if FLAGS.dataset == 'mnist':
+      dcgan = DCGAN(
+          sess,
+          input_width=FLAGS.input_width,
+          input_height=FLAGS.input_height,
+          output_width=FLAGS.output_width,
+          output_height=FLAGS.output_height,
+          batch_size=FLAGS.batch_size,
+          sample_num=FLAGS.batch_size,
+          y_dim=10,
+          dataset_name=FLAGS.dataset,
+          input_fname_pattern=FLAGS.input_fname_pattern,
+          crop=FLAGS.crop,
+          checkpoint_dir=checkpoint_dir,
+          sample_dir=sample_dir,
+					log_dir=log_dir,
+          blur_strategy=FLAGS.blur_strategy)
+    else:
+      dcgan = DCGAN(
+          sess,
+          input_width=FLAGS.input_width,
+          input_height=FLAGS.input_height,
+          output_width=FLAGS.output_width,
+          output_height=FLAGS.output_height,
+          batch_size=FLAGS.batch_size,
+          sample_num=FLAGS.batch_size,
+          dataset_name=FLAGS.dataset,
+          input_fname_pattern=FLAGS.input_fname_pattern,
+          crop=FLAGS.crop,
+          checkpoint_dir=checkpoint_dir,
+          sample_dir=sample_dir,
+					log_dir=log_dir,
+          blur_strategy=FLAGS.blur_strategy)
+
+    show_all_variables()
+
+    if FLAGS.train:
+      dcgan.train(FLAGS)
+    else:
+      if not dcgan.load(checkpoint_dir)[0]:
+        raise Exception("[!] Train a model first, then run test mode")
+      
+
+    # to_json("./web/js/layers.js", [dcgan.h0_w, dcgan.h0_b, dcgan.g_bn0],
+    #                 [dcgan.h1_w, dcgan.h1_b, dcgan.g_bn1],
+    #                 [dcgan.h2_w, dcgan.h2_b, dcgan.g_bn2],
+    #                 [dcgan.h3_w, dcgan.h3_b, dcgan.g_bn3],
+    #                 [dcgan.h4_w, dcgan.h4_b, None])
+  
+      generate_image_from_z(sess, dcgan, FLAGS)
+
+    # Below is codes for visualization
+    #OPTION = 1
+    #visualize(sess, dcgan, FLAGS, OPTION)
+
+if __name__ == '__main__':
+  tf.app.run()
---
+++ utils.py
@@ -0,0 +1,300 @@
+"""
+Some codes from https://github.com/Newmu/dcgan_code
+"""
+from __future__ import division
+import math
+import json
+import random
+import pprint
+import scipy.misc
+import numpy as np
+from time import gmtime, strftime
+from six.moves import xrange
+
+import os
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+
+pp = pprint.PrettyPrinter()
+
+get_stddev = lambda x, k_h, k_w: 1/math.sqrt(k_w*k_h*x.get_shape()[-1])
+
+def show_all_variables():
+  model_vars = tf.trainable_variables()
+  slim.model_analyzer.analyze_vars(model_vars, print_info=True)
+
+def get_image(image_path, input_height, input_width,
+              resize_height=64, resize_width=64,
+              crop=True, grayscale=False):
+  image = imread(image_path, grayscale)
+  return transform(image, input_height, input_width,
+                   resize_height, resize_width, crop)
+
+def save_images(images, size, image_path):
+  return imsave(inverse_transform(images), size, image_path)
+
+def save_images_ex(images, losses, size, image_path):
+  """
+  saves a batch of images to a file. Additionally, adds a green bar indicating the realism of the image
+  from the discriminators point of view. Images are sorted based on their realism.
+  """
+
+  losses = np.array(losses)
+
+  # parameters
+  _bar_width = 3
+  _bar_margin = 2
+  _bar_color = [-1.0, 1.0, -1.0] # green
+  _bar_opacity = 0.8
+
+  _img_width = _img_height = images.shape[1]
+  _max_bar_width = _img_width - 2*_bar_margin
+
+  max_loss_str=""
+
+  print("loss shape:", losses.shape)
+  losses = losses[:,:,0]
+
+  # add bar to images
+  for i in range(losses.shape[0]): # iterate trough the different losses we want to display [D_loss, C_loss]
+      
+    max_loss = np.max(losses[i])  # evaluate maximum loss of batch
+    relative_losses = losses[i]
+
+    for j in range(losses.shape[1]): # iterate through all losses in a batch
+      bar_width = (_bar_margin + int(_max_bar_width*relative_losses[j]))
+      images[j, (i+1)*_bar_margin + i*_bar_width : (i+1)*_bar_margin + (i+1)*_bar_width,	_bar_margin : bar_width] += np.multiply(_bar_color, _bar_opacity)
+
+    images = np.clip(images, -1.0, 1.0) # make sure green bar is not pushing image brightness over the limit	
+    max_loss_str += "_%3.3f" % max_loss
+
+  # sort images by D loss
+  sort_indices = np.argsort(losses[0], axis=0)
+  sort_indices = sort_indices.reshape(images.shape[0])
+
+  image_path = image_path.split(".png")
+  image_path = image_path[0] + max_loss_str + ".png"
+  images = images[sort_indices]
+  return imsave(inverse_transform(images), size, image_path)
+
+def imread(path, grayscale = False):
+  if (grayscale):
+    return scipy.misc.imread(path, flatten = True).astype(np.float)
+  else:
+    return scipy.misc.imread(path).astype(np.float)
+
+def merge_images(images, size):
+  return inverse_transform(images)
+
+def merge(images, size):
+  h, w = images.shape[1], images.shape[2]
+  if (images.shape[3] in (3,4)):
+    c = images.shape[3]
+    img = np.zeros((h * size[0], w * size[1], c))
+    for idx, image in enumerate(images):
+      i = idx % size[1]
+      j = idx // size[1]
+      img[j * h:j * h + h, i * w:i * w + w, :] = image
+    return img
+  elif images.shape[3]==1:
+    img = np.zeros((h * size[0], w * size[1]))
+    for idx, image in enumerate(images):
+      i = idx % size[1]
+      j = idx // size[1]
+      img[j * h:j * h + h, i * w:i * w + w] = image[:,:,0]
+    return img
+  else:
+    raise ValueError('in merge(images,size) images parameter '
+                     'must have dimensions: HxW or HxWx3 or HxWx4')
+
+def imsave(images, size, path):
+  image = np.squeeze(merge(images, size))
+  return scipy.misc.imsave(path, image)
+
+def center_crop(x, crop_h, crop_w,
+                resize_h=64, resize_w=64):
+  if crop_w is None:
+    crop_w = crop_h
+  h, w = x.shape[:2]
+  j = int(round((h - crop_h)/2.))
+  i = int(round((w - crop_w)/2.))
+  return scipy.misc.imresize(
+      x[j:j+crop_h, i:i+crop_w], [resize_h, resize_w])
+
+def transform(image, input_height, input_width, 
+              resize_height=64, resize_width=64, crop=True):
+  if crop:
+    cropped_image = center_crop(
+      image, input_height, input_width, 
+      resize_height, resize_width)
+  else:
+    cropped_image = scipy.misc.imresize(image, [resize_height, resize_width])
+  return np.array(cropped_image)/127.5 - 1.
+
+def inverse_transform(images):
+  return (images+1.)/2.
+
+def to_json(output_path, *layers):
+  with open(output_path, "w") as layer_f:
+    lines = ""
+    for w, b, bn in layers:
+      layer_idx = w.name.split('/')[0].split('h')[1]
+
+      B = b.eval()
+
+      if "lin/" in w.name:
+        W = w.eval()
+        depth = W.shape[1]
+      else:
+        W = np.rollaxis(w.eval(), 2, 0)
+        depth = W.shape[0]
+
+      biases = {"sy": 1, "sx": 1, "depth": depth, "w": ['%.2f' % elem for elem in list(B)]}
+      if bn != None:
+        gamma = bn.gamma.eval()
+        beta = bn.beta.eval()
+
+        gamma = {"sy": 1, "sx": 1, "depth": depth, "w": ['%.2f' % elem for elem in list(gamma)]}
+        beta = {"sy": 1, "sx": 1, "depth": depth, "w": ['%.2f' % elem for elem in list(beta)]}
+      else:
+        gamma = {"sy": 1, "sx": 1, "depth": 0, "w": []}
+        beta = {"sy": 1, "sx": 1, "depth": 0, "w": []}
+
+      if "lin/" in w.name:
+        fs = []
+        for w in W.T:
+          fs.append({"sy": 1, "sx": 1, "depth": W.shape[0], "w": ['%.2f' % elem for elem in list(w)]})
+
+        lines += """
+          var layer_%s = {
+            "layer_type": "fc", 
+            "sy": 1, "sx": 1, 
+            "out_sx": 1, "out_sy": 1,
+            "stride": 1, "pad": 0,
+            "out_depth": %s, "in_depth": %s,
+            "biases": %s,
+            "gamma": %s,
+            "beta": %s,
+            "filters": %s
+          };""" % (layer_idx.split('_')[0], W.shape[1], W.shape[0], biases, gamma, beta, fs)
+      else:
+        fs = []
+        for w_ in W:
+          fs.append({"sy": 5, "sx": 5, "depth": W.shape[3], "w": ['%.2f' % elem for elem in list(w_.flatten())]})
+
+        lines += """
+          var layer_%s = {
+            "layer_type": "deconv", 
+            "sy": 5, "sx": 5,
+            "out_sx": %s, "out_sy": %s,
+            "stride": 2, "pad": 1,
+            "out_depth": %s, "in_depth": %s,
+            "biases": %s,
+            "gamma": %s,
+            "beta": %s,
+            "filters": %s
+          };""" % (layer_idx, 2**(int(layer_idx)+2), 2**(int(layer_idx)+2),
+               W.shape[0], W.shape[3], biases, gamma, beta, fs)
+    layer_f.write(" ".join(lines.replace("'","").split()))
+
+def make_gif(images, fname, duration=2, true_image=False):
+  import moviepy.editor as mpy
+
+  def make_frame(t):
+    try:
+      x = images[int(len(images)/duration*t)]
+    except:
+      x = images[-1]
+
+    if true_image:
+      return x.astype(np.uint8)
+    else:
+      return ((x+1)/2*255).astype(np.uint8)
+
+  clip = mpy.VideoClip(make_frame, duration=duration)
+  clip.write_gif(fname, fps = len(images) / duration)
+
+def visualize(sess, dcgan, config, option):
+  image_frame_dim = int(math.ceil(config.batch_size**.5))
+  if option == 0:
+    z_sample = np.random.uniform(-0.5, 0.5, size=(config.batch_size, dcgan.z_dim))
+    samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
+    save_images(samples, [image_frame_dim, image_frame_dim], './samples/test_%s.png' % strftime("%Y%m%d%H%M%S", gmtime()))
+  elif option == 1:
+    values = np.arange(0, 1, 1./config.batch_size)
+    for idx in xrange(100):
+      print(" [*] %d" % idx)
+      z_sample = np.zeros([config.batch_size, dcgan.z_dim])
+      for kdx, z in enumerate(z_sample):
+        z[idx] = values[kdx]
+
+      if config.dataset == "mnist":
+        y = np.random.choice(10, config.batch_size)
+        y_one_hot = np.zeros((config.batch_size, 10))
+        y_one_hot[np.arange(config.batch_size), y] = 1
+
+        samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample, dcgan.y: y_one_hot})
+      else:
+        samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
+
+      save_images(samples, [image_frame_dim, image_frame_dim], './samples/test_arange_%s.png' % (idx))
+  elif option == 2:
+    values = np.arange(0, 1, 1./config.batch_size)
+    for idx in [random.randint(0, 99) for _ in xrange(100)]:
+      print(" [*] %d" % idx)
+      z = np.random.uniform(-0.2, 0.2, size=(dcgan.z_dim))
+      z_sample = np.tile(z, (config.batch_size, 1))
+      #z_sample = np.zeros([config.batch_size, dcgan.z_dim])
+      for kdx, z in enumerate(z_sample):
+        z[idx] = values[kdx]
+
+      if config.dataset == "mnist":
+        y = np.random.choice(10, config.batch_size)
+        y_one_hot = np.zeros((config.batch_size, 10))
+        y_one_hot[np.arange(config.batch_size), y] = 1
+
+        samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample, dcgan.y: y_one_hot})
+      else:
+        samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
+
+      try:
+        make_gif(samples, './samples/test_gif_%s.gif' % (idx))
+      except:
+        save_images(samples, [image_frame_dim, image_frame_dim], './samples/test_%s.png' % strftime("%Y%m%d%H%M%S", gmtime()))
+  elif option == 3:
+    values = np.arange(0, 1, 1./config.batch_size)
+    for idx in xrange(100):
+      print(" [*] %d" % idx)
+      z_sample = np.zeros([config.batch_size, dcgan.z_dim])
+      for kdx, z in enumerate(z_sample):
+        z[idx] = values[kdx]
+
+      samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
+      make_gif(samples, './samples/test_gif_%s.gif' % (idx))
+  elif option == 4:
+    image_set = []
+    values = np.arange(0, 1, 1./config.batch_size)
+
+    for idx in xrange(100):
+      print(" [*] %d" % idx)
+      z_sample = np.zeros([config.batch_size, dcgan.z_dim])
+      for kdx, z in enumerate(z_sample): z[idx] = values[kdx]
+
+      image_set.append(sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample}))
+      make_gif(image_set[-1], './samples/test_gif_%s.gif' % (idx))
+
+    new_image_set = [merge(np.array([images[idx] for images in image_set]), [10, 10]) \
+        for idx in range(64) + range(63, -1, -1)]
+    make_gif(new_image_set, './samples/test_gif_merged.gif', duration=8)
+
+def generate_image_from_z(sess, dcgan, config):
+  batch_z = np.loadtxt(config.inference_z_src)
+
+  if not os.path.exists('generated'):
+    os.makedirs('generated')
+
+  samples = sess.run(dcgan.sampler,
+                    feed_dict={
+                      dcgan.z: batch_z
+                      })
+  save_images(samples, [8,8], './generated/test_{}.png'.format(config.folder_suffix))
